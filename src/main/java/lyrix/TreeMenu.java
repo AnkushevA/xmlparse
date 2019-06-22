e4fa60341d132d42775fd855b80dfe65dee1f19c
ankusheva <ankush3v@mail.ru>
2019-06-22 19:28:32 +0300

    Create SOAP message

--- src/main/java/lyrix/TreeMenu.java
+++ src/main/java/lyrix/TreeMenu.java
@@ -204,35 +204,4 @@ public class TreeMenu extends JPanel {
             }
         }
     }
-
-    void createSoapMessage() {
-        try {
-            MessageFactory factory = MessageFactory.newInstance();
-            SOAPMessage soapMsg = factory.createMessage();
-            SOAPPart part = soapMsg.getSOAPPart();
-
-            SOAPEnvelope envelope = part.getEnvelope();
-            SOAPHeader header = envelope.getHeader();
-            SOAPBody body = envelope.getBody();
-
-//            header.addTextNode("Training Details");
-
-            SOAPBodyElement element = body.addBodyElement(envelope.createName("JAVA", "training", "https://jitendrazaa.com/blog"));
-            element.addChildElement("WS").addTextNode("Training on Web service");
-
-            SOAPBodyElement element1 = body.addBodyElement(envelope.createName("JAVA", "training", "https://jitendrazaa.com/blog"));
-            element1.addChildElement("Spring").addTextNode("Training on Spring 3.0");
-
-            soapMsg.writeTo(System.out);
-
-            FileOutputStream fOut = new FileOutputStream("SoapMessage.xml");
-            soapMsg.writeTo(fOut);
-
-            System.out.println();
-            System.out.println("SOAP msg created");
-
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-    }
 }
